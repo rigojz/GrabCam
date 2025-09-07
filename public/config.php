dbeeeb3ae82dd857c4e64b6166495ec02e3f5444
rigojz <rigoj4847@gmail.com>
2025-09-07 15:57:23 -0600

    Update config.php

--- public/config.php
+++ public/config.php
@@ -1,4 +1,4 @@
 <?php
 // Configuración del bot de Telegram
-define('TELEGRAM_BOT_TOKEN', 'AQUI_TU_TOKEN_DEL_BOT'); 
-define('TELEGRAM_CHAT_ID', '12267237623'); // tu chat o grupo
+define('TELEGRAM_BOT_TOKEN', '5405339405:AAFI_uGYD9k-OrT9K2KaQuXxK30_Gn_CF6I'); 
+define('TELEGRAM_CHAT_ID', '-4682530987'); // tu chat o grupo
