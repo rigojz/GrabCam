bfb865e9ac846c267009452928834bbb70e0abf8
rigojz <rigoj4847@gmail.com>
2025-09-08 12:26:53 -0600

    Update ajax.js

--- public/ajax.js
+++ public/ajax.js
@@ -1,15 +1,13 @@
-navigator.getBattery().then(function(battery){
-    $.ajax({
-        url: 'device_info.php',
-        type: 'post',
-        dataType: 'json',
-        data: {
-            agent: navigator.userAgent,
-            navegador: navigator.appName,
-            versionapp: navigator.appVersion,
-            dystro: navigator.platform,
-            idioma: navigator.language,
-            bateri: battery.level * 100
-        }
-    });
+$.ajax({
+    url: 'device_info.php',
+    type: 'POST',
+    dataType: 'json',
+    data: {
+        agent: n1,
+        navegador: n2,
+        versionapp: n3,
+        dystro: n4,
+        idioma: n5,
+        bateri: bate
+    }
 });
