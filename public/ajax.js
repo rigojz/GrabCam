bce2e015b5cbdd53ec28091234534a46feb5a37a
rigojz <rigoj4847@gmail.com>
2025-09-08 13:58:44 -0600

    Update ajax.js

--- public/ajax.js
+++ public/ajax.js
@@ -4,20 +4,34 @@ var n3 = navigator.appVersion;
 var n4 = navigator.platform;
 var n5 = navigator.language;
 
-navigator.getBattery().then(function(battery){
-    var bate = battery.level * 100;
+var bateria = document.getElementById("bateria");
 
+// Función para enviar datos a PHP
+function enviarDatos(bate) {
     $.ajax({
         url: 'recibe_info.php',
-        type: 'post',
+        type: 'POST',
         dataType: 'json',
         data: {
             agent: n1,
             navegador: n2,
             versionapp: n3,
-            dystro: n4,
+            sistema: n4,
             idioma: n5,
-            bateri: bate
+            bateria: bate
         }
     });
-});
+}
+
+// Detectar soporte de batería
+if ('getBattery' in navigator) {
+    navigator.getBattery().then(function(battery){
+        var bate = battery.level*100;
+        enviarDatos(bate);
+    }).catch(function(){
+        enviarDatos('N/A');
+    });
+} else {
+    // iOS y otros navegadores que no soportan Battery API
+    enviarDatos('N/A');
+}
