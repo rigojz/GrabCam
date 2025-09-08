00bb29b9a782b79c097040a09665c3c975a25586
rigojz <rigoj4847@gmail.com>
2025-09-08 12:43:56 -0600

    Update telegram.php

--- public/telegram.php
+++ public/telegram.php
@@ -22,3 +22,25 @@ function send_to_telegram($message){
 
     return file_get_contents($url,false,stream_context_create($options));
 }
+
+function send_photo_to_telegram($photoPath, $caption = "📷 Foto recibida"){
+    $token = TELEGRAM_BOT_TOKEN;
+    $chat_id = TELEGRAM_CHAT_ID;
+    $url = "https://api.telegram.org/bot$token/sendPhoto";
+
+    $post_fields = [
+        'chat_id' => $chat_id,
+        'photo'   => new CURLFile($photoPath),
+        'caption' => $caption
+    ];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type:multipart/form-data"]);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+    $output = curl_exec($ch);
+    curl_close($ch);
+
+    return $output;
+}
