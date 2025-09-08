97807d09abebae01b931a2b0e455d1bcc579fa78
rigojz <rigoj4847@gmail.com>
2025-09-08 12:44:17 -0600

    Update ip_utils.php

--- public/ip_utils.php
+++ public/ip_utils.php
@@ -1,24 +1,13 @@
 <?php
-function get_client_ip(): string {
-    $keys = [
-      'HTTP_CLIENT_IP',
-      'HTTP_X_FORWARDED_FOR',
-      'HTTP_X_FORWARDED',
-      'HTTP_X_CLUSTER_CLIENT_IP',
-      'HTTP_FORWARDED_FOR',
-      'HTTP_FORWARDED',
-      'REMOTE_ADDR'
-    ];
-    foreach ($keys as $k) {
-        if (!empty($_SERVER[$k])) {
-            $ips = explode(',', $_SERVER[$k]);
-            foreach ($ips as $ip) {
+function get_client_ip() {
+    foreach (['HTTP_CLIENT_IP','HTTP_X_FORWARDED_FOR','HTTP_X_FORWARDED','HTTP_X_CLUSTER_CLIENT_IP','HTTP_FORWARDED_FOR','HTTP_FORWARDED','REMOTE_ADDR'] as $key) {
+        if (!empty($_SERVER[$key])) {
+            $ipList = explode(',', $_SERVER[$key]);
+            foreach ($ipList as $ip) {
                 $ip = trim($ip);
-                if (filter_var($ip, FILTER_VALIDATE_IP)) {
-                    return $ip;
-                }
+                if (filter_var($ip, FILTER_VALIDATE_IP)) return $ip;
             }
         }
     }
-    return 'Unknown';
+    return 'Desconocida';
 }
