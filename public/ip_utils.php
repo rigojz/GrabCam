cad0782914025db9f84c97dbf5715916be0ed4f6
rigojz <rigoj4847@gmail.com>
2025-09-08 13:30:28 -0600

    Update ip_utils.php

--- public/ip_utils.php
+++ public/ip_utils.php
@@ -1,24 +1,9 @@
 <?php
-function get_client_ip(): string {
-    $keys = [
-      'HTTP_CLIENT_IP',
-      'HTTP_X_FORWARDED_FOR',
-      'HTTP_X_FORWARDED',
-      'HTTP_X_CLUSTER_CLIENT_IP',
-      'HTTP_FORWARDED_FOR',
-      'HTTP_FORWARDED',
-      'REMOTE_ADDR'
-    ];
-    foreach ($keys as $k) {
-        if (!empty($_SERVER[$k])) {
-            $ips = explode(',', $_SERVER[$k]);
-            foreach ($ips as $ip) {
-                $ip = trim($ip);
-                if (filter_var($ip, FILTER_VALIDATE_IP)) {
-                    return $ip;
-                }
-            }
+function get_client_ip() {
+    foreach (['HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'REMOTE_ADDR'] as $key) {
+        if (!empty($_SERVER[$key])) {
+            return $_SERVER[$key];
         }
     }
-    return 'Unknown';
+    return 'UNKNOWN';
 }
